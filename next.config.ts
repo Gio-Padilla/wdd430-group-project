4fa973a87d219cf4b68584599f89efe008048f3d
pdmoura <sudwarriormoura@gmail.com>
2026-05-24 01:56:36 -0300

    chore: update pnpm workspace build configuration and define turbopack root directory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "E:/wdd430-group-project-1",
+  },
 };
 
 export default nextConfig;
